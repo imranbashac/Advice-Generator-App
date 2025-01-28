8d0e6fa4e0abdbc796f873f02eb26f7fbd1103f2
Imran Basha <93580719+imranbashac@users.noreply.github.com>
2025-01-28 16:07:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,11 @@ const adviceNumber = document.querySelector('.advice-number');
 const adviceText = document.querySelector('.advice-text');
 const adviceBtn = document.querySelector('.dice-btn');
 
-function delay(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
 async function getAdvice() {
     adviceText.textContent = 'Loading...';
-    await delay(500);
-
     try {
-        // Append a timestamp to the URL
         const response = await fetch(`https://api.adviceslip.com/advice?timestamp=${Date.now()}`, {
-            cache: "no-cache", // Ensure the browser doesn't cache
+            cache: "no-cache", 
         });
 
         const data = await response.json();
